683cfac25d6c124295afda65ccc99d79fa2f2d6e
JaredN05 <jaredleenaicker@gmail.com>
2023-06-18 11:55:07 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/*
+ * Task: 3. alphABET
+ * Creator: Jared Naicker
+ */
+
+/**
+ * main - Displays the alphabet in lowercase and uppercase, using the for loop.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		putchar(alphabet);
+
+        for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
+		putchar(alphabet);
+
+	putchar('\n');
+
+	return (0);
+}
