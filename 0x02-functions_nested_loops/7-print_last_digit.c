25ad26ffb531701f219d71c4093bfa6c9ac475c0
JaredN05 <jaredleenaicker@gmail.com>
2023-06-20 14:15:46 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/*
+ * Task: 7. There are only 3 colors, 10 digits, and 7 notes;
+ * 	    it's what we do with them that's important.
+ * Creator: Jared Naicker
+ */
+
+/**
+ * print_last_digit - Prints the last digit of a number.
+ * @n: The number in question. 
+ *
+ * Return: Value of the last digit.
+ */
+int print_last_digit(int n)
+{
+	int last_digit = n % 10;
+
+	if (last_digit < 0)
+		last_digit *= -1;
+
+	_putchar(last_digit + '0');
+
+	return (last_digit);
+}
