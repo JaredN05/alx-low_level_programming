4d477b24984c12b1aa09861928c13113d96b1106
JaredN05 <jaredleenaicker@gmail.com>
2023-06-22 07:35:03 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/*
+ * Task: 2. Collaboration is multiplication
+ * Creator: Jared Naicker
+ */
+
+/**
+ * mul - Multiples two integers.
+ * @a: The first integer to be multiplied.
+ * @b: The second integer to be multiplied.
+ *
+ * Return: The result of the multiplication.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
