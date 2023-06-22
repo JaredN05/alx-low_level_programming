28389b11f392b837a85d81577acd1be218e2294e
JaredN05 <jaredleenaicker@gmail.com>
2023-06-22 12:04:35 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/*
+ * Task: 6. The shortest distance between two points is a straight line
+ * Creator: Jared Naicker
+ */
+
+/**
+ * print_line - Prints a straight line using the character _.
+ * @n: The number of _ characters to be printed.
+ */
+void print_line(int n)
+{
+	int length;
+
+	if (n > 0)
+	{
+		for (length = 0; length < n; length++)
+			_putchar('_');
+	}
+
+	_putchar('\n');
+}
