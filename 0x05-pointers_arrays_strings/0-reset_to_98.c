77d01959ca5a3fc056517c2fa95f0e38e09b0e82
JaredN05 <jaredleenaicker@gmail.com>
2023-06-26 11:53:09 -0700

    0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/*
+ * Task: 0. 98 Battery st
+ * Creator: Jared Naicker
+ */
+
+/**
+ * reset_to_98 - Takes a pointer to an int and updates
+ *               the value it points to 98.
+ * @n: The pointer to an int.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
