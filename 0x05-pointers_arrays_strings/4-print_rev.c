615bab7a4a1621534a7c72d5ed55e2c3e5df49b8
JaredN05 <jaredleenaicker@gmail.com>
2023-06-26 12:44:49 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/*
+ * Task: 4. I can only go one way. I've not got a reverse gear
+ * Creator: Jared Naicker
+ */
+
+/**
+ * print_rev - Prints a string in reverse.
+ * @s: The string to be printed.
+ */
+void print_rev(char *s)
+{
+	int len = 0, index;
+
+	while (s[index++])
+		len++;
+
+	for (index = len - 1; index >= 0; index--)
+		_putchar(s[index]);
+
+	_putchar('\n');
+}
