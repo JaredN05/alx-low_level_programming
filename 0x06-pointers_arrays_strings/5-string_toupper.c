84328f13f04eb7760fa8d9f2559d25430ffc97bb
JaredN05 <jaredleenaicker@gmail.com>
2023-06-28 13:35:43 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/*
+ * Task: 5. Always look up
+ * Creator: Jared Naicker
+ */
+
+/**
+  * string_toupper - Changes all lowercase letters to uppercase
+  * @p: The string will be modified
+  *
+  * Return: char var
+  */
+char *string_toupper(char *)
+{
+	int a = 0;
+
+	while (p[a])
+	{
+		if (p[a] >= 97 && p[a] <= 122)
+		{
+			p[a] -= 32;
+		}
+
+		a++;
+	}
+
+	return (p);
+}
