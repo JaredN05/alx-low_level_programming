323437d1708998302d2a62cdb2b6e50dfffe1336
JaredN05 <jaredleenaicker@gmail.com>
2023-07-03 10:58:15 -0700

    7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/*
+ * Task: 6. Chess is mental torture
+ * Creator: Jared Naicker
+ */
+
+/**
+  * print_chessboard - Print the chessboard
+  * @a: array of pieces
+  *
+  * Return: Nothing.
+  */
+void print_chessboard(char (*a)[8])
+{
+	int b, c;
+
+	for (b = 0; b < 8; b++)
+	{
+		for (c = 0; c < 8; c++)
+		{
+			_putchar(a[b][c]);
+		}
+
+		_putchar('\n');
+	}
+}
