cfb7049c2741098b3185bdd095a15ccf402b049e
JaredN05 <jaredleenaicker@gmail.com>
2023-07-05 01:53:48 -0700

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/*
+ * Task: 2. Dreams feel real while we're in them
+ * Creator: Jared Naicker
+ */
+
+/**
+  * _strlen_recursion - Calculate the length of a string
+  * @s: the string to count
+  *
+  * Return: integer value
+  */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+
+	s++;
+	return (_strlen_recursion(s) + 1);
+}
