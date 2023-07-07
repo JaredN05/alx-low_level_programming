1012f46914d7d737c45fe0dadc693afcf35643b7
JaredN05 <jaredleenaicker@gmail.com>
2023-07-07 13:28:59 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/*
+ * Task: 0. It ain't what they call you, it's what you answer to
+ * Creator: Jared Naicker
+ */
+
+/**
+  * main - Prints its name
+  * @argc: argument count
+  * @argv: argument vector
+  *
+  * Return: Always zero
+  */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
