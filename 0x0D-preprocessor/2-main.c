67b229c536db9a2011556147b8596a0c8f213a1d
JaredN05 <jaredleenaicker@gmail.com>
2023-07-17 09:55:48 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+ * Task: 2. File name
+ * Creator: Jared Naicker
+ */
+
+/**
+  * main - Prints the file name of the program
+  *
+  * Return: Always zero
+  */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
