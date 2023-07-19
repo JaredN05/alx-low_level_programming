0a250d08402fe704454e356232f160605a85f84d
JaredN05 <jaredleenaicker@gmail.com>
2023-07-19 11:03:24 -0700

    Adding first task files and README.md file

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+
+/*
+ * Task: 0. What's my name
+ * Creator: Jared Naicker
+ */
+
+/**
+  * print_name - Prints a name
+  * @name: The name to prints
+  * @f: Pointer to function
+  *
+  * Return: Nothing
+  */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
