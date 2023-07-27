24b0fc7f117061474f89907da7595b6ad724cbf2
JaredN05 <jaredleenaicker@gmail.com>
2023-07-27 03:35:52 -0700

    2-add_node.c

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/*
+ * Task: 2. Add node
+ * Creator: Jared Naicker
+ */
+
+/**
+  * add_node - Adds a new node at the beginning of a list
+  * @head: The original linked list
+  * @str: The string to add to the node
+  *
+  * Return: The address of the new list or NULL if it failed
+  */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	unsigned int len = 0;
+
+	while (str[len])
+		len++;
+
+	new = malloc(sizeof(list_t));
+	if (!new)
+		return (NULL);
+
+	new->str = strdup(str);
+	new->len = len;
+	new->next = (*head);
+	(*head) = new;
+
+	return (*head);
+}
