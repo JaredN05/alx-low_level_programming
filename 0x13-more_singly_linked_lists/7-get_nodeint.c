0e36ff285a47764c90dd637edcb7ad18083a4077
JaredN05 <jaredleenaicker@gmail.com>
2023-08-01 06:11:28 -0700

    7-get_nodeint.c

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/*
+ * Task: 7. Get node at index
+ * Creator: Jared Naicker
+ */
+
+/**
+  * get_nodeint_at_index - ...
+  * @head: ...
+  * @index: ...
+  *
+  * Return: ...
+  */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int count = 0;
+
+	if (head)
+	{
+		while (head)
+		{
+			if (count == index)
+				return (head);
+
+			head = head->next;
+			count++;
+		}
+	}
+
+	return (NULL);
+}
